fcfaaef28b4fe06e8b8f72cb4b6c708eea11ed4e
Justin <justin-urbanek@bluewin.ch>
2024-07-12 14:13:31 +0200

    feat: page routing with index page

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>
+      <h1>Welcome to the home page</h1>
+      <p>Here you can find all the information you need to know about our company.</p>
+    </div>
+  </div>
+</template>
