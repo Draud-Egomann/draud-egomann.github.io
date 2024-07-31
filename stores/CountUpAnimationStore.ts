aa4fd9c4409cc3e8049b152d2081177d5d47f773
Justin <justin-urbanek@bluewin.ch>
2024-07-31 16:08:42 +0200

    feat: countUpAnimationStore

---
+++ stores/CountUpAnimationStore.ts
@@ -0,0 +1,22 @@
+import { gsap } from "gsap";
+
+export const useCountUpAnimationStore = defineStore("countUpAnimation", () => {
+  const number = ref(0);
+  const counter = reactive({
+    number: 0,
+  });
+
+  watch(number, (n: number): void => {
+    gsap.to(counter, { duration: 2, number: n || 0 });
+  });
+
+  function setNumber(n: number) {
+    number.value = n;
+  }
+
+  return {
+    number,
+    counter,
+    setNumber,
+  };
+});
