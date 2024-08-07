63e91f1480354558bbe6bde79795d71d6a0adb97
Justin <justin-urbanek@bluewin.ch>
2024-08-07 21:22:13 +0200

    feat: ContentGrid component and implementation

---
+++ components/Grids/ContentGrid.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+import { MediaItem } from '#imports';
+
+const props = defineProps<{
+  title: string;
+  mainText: string;
+  mediaItems: MediaItem[];
+}>();
+
+const justifyWithIndex = (index: number): string => {
+  return index % 2 == 0 ? 'lg:justify-end' : 'lg:justify-start';
+}
+</script>
+
+<template>
+  <div class="w-full relative bg-ownSecondary px-8">
+    <div class="container flex flex-wrap lg:flex-nowrap justify-center items-center gap-8 py-12 mx-auto">
+      <div class="w-full lg:w-1/4 flex flex-col items-center gap-8 px-12 lg:px-0">
+        <h2 class="text-3xl text-center text-white mx-4 font-bold">
+          {{ title }}
+        </h2>
+
+        <p class="w-full lg:max-w-md text-center lg:text-left text-white">
+          {{ mainText }}
+        </p>
+      </div>
+
+      <div class="w-full lg:w-3/4 flex flex-wrap justify-center">
+
+        <div v-for="item in props.mediaItems" class="w-full sm:w-1/2 p-4 md:p-8 flex justify-center items-center"
+          :class="justifyWithIndex(props.mediaItems.indexOf(item))">
+          <div class="relative bg-gray-600 rounded-lg w-full max-w-[368px] h-[256px] overflow-hidden">
+
+            <NuxtLink :to="item.LinkUrl ?? '/'">
+              <button class="relative w-full h-full rounded-lg overflow-hidden p-8">
+                <!-- Separate text and image to separate hover effects -->
+                <div :class="item.ImageClass" :style="`background-image: url(${item.ImageUrl});`"
+                  class="absolute inset-0 bgImg transform-gpu duration-300 hover:scale-125">
+                </div>
+
+                <div v-if="!stringHelper.IsNullOrEmpty(item.Title)"
+                  class="absolute bottom-8 left-8 flex items-center gap-4">
+                  <fa-icon icon="arrow-right" class="text-white" />
+
+                  <p class="text-xl text-white">
+                    {{ item.Title }}
+                  </p>
+                </div>
+              </button>
+            </NuxtLink>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { ImageBaseItem } from '~/composables/models/ImageBaseItem';
+import { MediaItem } from '~/composables/models/MediaItem';
 
 const birthDate: Date = new Date(2004, 12, 17);
 const mySelfImage: ImageBaseItem = new ImageBaseItem("/imgs/me.png", null, "Image of Myself");
 const catchphraseText: string = "-jähriger angehender Entwickler mit einer Leidenschaft für Informatik";
 const diff = new Date().getFullYear() - birthDate.getFullYear();
+
+const gridContentItems: MediaItem[] = [
+  new MediaItem("imgs/me.png", "Titel", "Beschreibung", "https://www.google.com", null, "altText", null, null),
+  new MediaItem("imgs/me.png", "Titel", "Beschreibung", "https://www.google.com", null, "altText", null, null),
+  new MediaItem("imgs/me.png", "Titel", "Beschreibung", "https://www.google.com", null, "altText", null, null),
+  new MediaItem("imgs/me.png", "Titel", "Beschreibung", "https://www.google.com", null, "altText", null, null),
+];
 
 function NavigateToAbout() {
   const router = useRouter();
@@ -14,7 +22,7 @@ function NavigateToAbout() {
 
 <template>
   <div>
-    <HeroBanner :title="'Justin Urbanek - Ich'"
+    <LazyHeroBanner :title="'Justin Urbanek - Ich'"
                 :subTitle="null"
                 :paragraphs="['Willkommen auf meiner persönlichen Webseite! Ich bin Justin Urbanek, ein leidenschaftlicher Informatiklernender im dritten Lehrjahr.', 'Aktuell absolviere ich meine Ausbildung bei der Kauz Informatik Medien AG und besuche das Berufsbildungszentrum BBZW Sursee.']"
                 :hasButton="true"
@@ -23,6 +31,11 @@ function NavigateToAbout() {
                 @buttonClickEvent="NavigateToAbout"
     />
 
-    <TextCatchphrase :ImageItem="mySelfImage" :Text="catchphraseText" :Age="diff" />
+    <LazyTextCatchphrase :ImageItem="mySelfImage" :Text="catchphraseText" :Age="diff" />
+
+    <LazyGridsContentGrid :title="'Erfahrung'"
+                      :main-text="'Im Laufe meiner Ausbildung und beruflichen Tätigkeit habe ich an einer Vielzahl von Projekten gearbeitet, von Schulprojekten, Arbeitsprojekten, privaten Projekten bis hin Hackathon-Projekten. Meine Markanntesten Projekte sind hier einsehbar.'"
+                      :mediaItems="gridContentItems"
+    />
   </div>
 </template>
