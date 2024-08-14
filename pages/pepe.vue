7770caf51efa435b150b94e8db1a109e64be1736
Justin <justin-urbanek@bluewin.ch>
2024-08-14 18:05:26 +0200

    feat: pepe

---
+++ pages/pepe.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <LazyHeroBanner :title="'Pepe?'"
+                    :subTitle="'Pepe.'"
+                    :paragraphs="[]"
+                    :hasButton="false"
+                    :isBigView="false"
+                    :buttonText="null"
+                    @buttonClickEvent="() => { }" />
+
+    <img src="https://c.tenor.com/3kPGZrOLeiUAAAAC/tenor.gif" class="mx-auto"/>
+  </div>
+</template>
