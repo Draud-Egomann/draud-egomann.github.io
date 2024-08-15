f77657de89a037f9257f5a853925db4a3be2d642
egomann88 <justin-urbanek@bluewin.ch>
2024-08-15 21:02:46 +0200

    feat: better pepe gif and link to source

--- pages/pepe.vue
+++ pages/pepe.vue
@@ -8,6 +8,23 @@
                     :buttonText="null"
                     @buttonClickEvent="() => { }" />
 
-    <img src="https://c.tenor.com/3kPGZrOLeiUAAAAC/tenor.gif" class="mx-auto"/>
+    <img src="https://media.tenor.com/Km4KsS1PixgAAAAi/pepe-the-frog-dancing.gif" class="mx-auto w-[52vh]" />
+
+    <div class="text-white text-center my-2">
+      Bild von
+      <NuxtLink to="https://tenor.com/de/users/%E3%83%AD%E3%83%9F%E3%83%8E%E3%82%B9%E3%83%88%E3%83%A9" class="link">
+        ロミノストラ
+      </NuxtLink>
+      auf
+      <NuxtLink to="https://tenor.com/de/view/pepe-the-frog-dancing-gif-20649836" class="link">
+        tenor
+      </NuxtLink>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.link {
+  @apply text-white underline;
+}
+</style>
