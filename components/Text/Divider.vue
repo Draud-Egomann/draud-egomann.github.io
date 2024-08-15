3b472a49b7638777decd8c4325673551d94dfac0
egomann88 <justin-urbanek@bluewin.ch>
2024-08-15 20:26:26 +0200

    feat: Divider component

---
+++ components/Text/Divider.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  text: string;
+}>();
+</script>
+
+<template>
+  <div class="w-full relative bg-ownPrimary">
+    <div class="container flex flex-wrap justify-center items-center gap-8 py-12 mx-auto">
+      <div class="w-1/2 h-1 bg-gradient-to-r from-transparent via-gray-700/80 to-transparent my-2 rounded-xl"></div>
+      <p class="text-white w-full text-center">
+        {{ text }}
+      </p>
+    </div>
+  </div>
+</template>
