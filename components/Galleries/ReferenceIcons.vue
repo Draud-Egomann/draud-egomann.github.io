83419d37579f35e270ea3432b44e9453bee28c63
egomann88 <justin-urbanek@bluewin.ch>
2024-08-15 20:24:30 +0200

    feat: ReferenceIcons components

---
+++ components/Galleries/ReferenceIcons.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { MediaItem } from '#imports';
+
+defineProps<{
+  icons: MediaItem[];
+}>();
+</script>
+
+<template>
+  <div class="w-full relative own-bg-primary">
+    <div class="container flex flex-wrap justify-center items-center gap-8 py-12 mx-auto">
+      <GalleriesReferenceIcon v-for="icon in icons" :icon="icon" />
+    </div>
+  </div>
+</template>
