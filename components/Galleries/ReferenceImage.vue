812e75ee1709f0f8ad6fd31ed443fd944d946133
egomann88 <justin-urbanek@bluewin.ch>
2024-08-15 18:42:07 +0200

    feat: referenceImage(s) component(s)

---
+++ components/Galleries/ReferenceImage.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { ImageReference } from '~/composables/models/ImageReference';
+
+defineProps<{
+  imageReference: ImageReference;
+}>();
+</script>
+
+<template>
+  <div class="w-full sm:w-1/3">
+    <img :src="imageReference.ImageUrl" :alt="imageReference.ImageAltText ?? ''" :class="imageReference.ImageClass"
+      class="mx-auto" aria-hidden="true" />
+
+    <p class="text-white">
+      Bild von
+
+    <div v-if="imageReference.ProfileUrl" class="inline-block">
+      <NuxtLink :to="imageReference.ProfileUrl" target="_blank" rel="nofollow" class="link">
+        {{ imageReference.ProfileName + " " }}
+      </NuxtLink>
+      <span>auf</span>
+    </div>
+
+    <NuxtLink :to="imageReference.SiteUrl ?? ''" target="_blank" rel="nofollow" class="link">
+      {{ " " + imageReference.SiteName }}
+    </NuxtLink>
+    </p>
+  </div>
+</template>
+
+<style>
+.link {
+  @apply text-white no-underline;
+}
+</style>
---
+++ components/Galleries/ReferenceImages.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { ImageReference } from '~/composables/models/ImageReference';
+
+defineProps<{
+  imageReferences: ImageReference[];
+}>();
+</script>
+
+<template>
+  <div class="w-full relative bg-ownPrimary">
+    <div class="container flex flex-wrap justify-center items-center gap-8 py-12 mx-auto">
+      <GalleriesReferenceImage v-for="imageReference in imageReferences" :imageReference="imageReference" />
+    </div>
+  </div>
+</template>
