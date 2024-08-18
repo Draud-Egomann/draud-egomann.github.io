eafb00dd71c9d56ac905ce0c743f7e7dff56ce99
egomann88 <justin-urbanek@bluewin.ch>
2024-08-18 18:27:20 +0200

    feat: references animations

--- components/Galleries/ReferenceImages.vue
+++ components/Galleries/ReferenceImages.vue
@@ -7,7 +7,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="w-full relative bg-ownPrimary">
+  <div class="w-full relative">
     <div class="container flex flex-wrap justify-center items-center gap-8 py-12 mx-auto">
       <GalleriesReferenceImage v-for="imageReference, index in imageReferences" :imageReference="imageReference"
         data-aos="zoom-in" />
