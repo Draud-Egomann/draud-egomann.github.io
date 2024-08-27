b79ffba8d7c3e04c152d0db479efb14919a101b5
Justin <justin-urbanek@bluewin.ch>
2024-08-27 08:53:36 +0200

    feat: remove reference from pepe meme

--- pages/pepe.vue
+++ pages/pepe.vue
@@ -7,25 +7,6 @@
         :buttonText="null" @buttonClickEvent="() => { }" />
 
       <img src="/imgs/eastereggs/pepe.gif" class="mx-auto w-[52vh]" />
-
-      <div class="text-white text-center my-2">
-        {{ $t('components.imgFrom') }}
-        <NuxtLink to="https://tenor.com/de/users/%E3%83%AD%E3%83%9F%E3%83%8E%E3%82%B9%E3%83%88%E3%83%A9" class="link"
-          target="_blank" rel="nofollow">
-          ロミノストラ
-        </NuxtLink>
-        {{ $t('components.imgOn') }}
-        <NuxtLink to="https://tenor.com/de/view/pepe-the-frog-dancing-gif-20649836" class="link" target="_blank"
-          rel="nofollow">
-          tenor
-        </NuxtLink>
-      </div>
     </RenderCacheable>
   </div>
 </template>
-
-<style scoped>
-.link {
-  @apply text-white underline;
-}
-</style>
